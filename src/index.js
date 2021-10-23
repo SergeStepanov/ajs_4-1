440c3530dd95f9fb5a1dfcae5c8c4ad298f8e311
Sergey Stepanov <ser1653285@yandex.ru>
2021-10-23 20:28:36 +0300

    added all

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@
 /* eslint-disable no-undef */
 
 export default function totalLive(obj) {
-  for (const iterator of obj) {
-    if (iterator.health <= 15) {
-      return critical;
-    }
-    if (iterator.health >= 50) {
-      return healthy;
-    }
-    return wounded;
+  if (obj.health <= 15) {
+    return critical;
   }
+  if (obj.health >= 50) {
+    return healthy;
+  }
+  return wounded;
 }
